ba528825018b837a5ece9bb2088618691f17a6d0
AntonioMennuni <antoniomennuni97@gmail.com>
2023-06-24 01:50:24 +0200

    Ho aggiunto la classe VisualizzatoreFrasi per la visualizzazione corretta dei dialoghi

--- src/main/java/com/mycompany/avventura/Engine.java
+++ src/main/java/com/mycompany/avventura/Engine.java
@@ -9,10 +9,18 @@ import com.mycompany.gioco.Avventura;
 import com.mycompany.parser.Parser;
 import com.mycompany.parser.ParserOutput;
 import com.mycompany.tipi.TipoComando;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -63,50 +71,53 @@ public class Engine
     //Questo metodo fa partire il gioco, rendendolo giocabile all'utente
     public void esegui()
     {
-        System.out.println("================================");
-        System.out.println("The last of us PART 1");
-        System.out.println("================================");
+        //frasi introduttive
+        List<String> frasi = new ArrayList<>();
+        frasi.add("Prod: Antonio Mennuni , Danilo Santo , Gabriele Nigro");
+        frasi.add("================================");
+        frasi.add("The last of us PART 1");
+        frasi.add("================================");
+        frasi.add("Siamo nel 2050 e 37 anni fa, esattamente il 26/09/2013, un fungo chiamato cordyceps");
+        frasi.add("ha causato una pandemia globale che ha infettato il 90% della popolazione mondiale,");
+        frasi.add("trasformandola in bestie assetate di sangue (simili a zombie) che attaccano e sbranano chiunque gli si ponga davanti.");
+        frasi.add("Nel frattempo l'esercito ha creato delle ZQ, cioè delle aree murate in cui la popolazione ha ripreso a vivere");
+        frasi.add("e in cui vige il pugno di ferro dell'esercito. Siamo nella ZQ di Boston e un contrabbandiere di nome Joel deve recuperare");
+        frasi.add("un carico di armi che gli è stato rubato da un tale Robert, un truffatore noto nella ZQ. Dopo una iniziale colluttazione,");
+        frasi.add("Robert confessa a Joel che le sue armi sono state vendute alle luci. Chi sono le luci? Le luci sono un gruppo di resistenza");
+        frasi.add("(da molti definiti terroristi) che si oppone all'oppressione dell'esercito e cerca di trovare una cura per la pandemia causata dal fungo.");
+        frasi.add("Saputo ciò, Joel incontra il capo delle luci, una tale Marlene, e spiega l'accaduto per riavere indietro le sue armi.");
+        frasi.add("Marlene propone a Joel un patto: deve portare fuori dalla ZQ, in uno stabilimento delle luci a Nord, una bambina di 12 anni di nome Ellie.");
+        frasi.add("Joel decide, dopo un iniziale rifiuto, di accettare il patto e organizza così il viaggio...");
+        frasi.add("\n");
         
-        System.out.println("""
-                           Siamo nel 2050 e 37 anni fa, esattamente il 26/09/2013, un fungo chiamato cordyceps
-                           ha causato una pandemia globale che ha infettato il 90% della popolazione mondiale,
-                           trasformandola in bestie assetate di sangue (simili a zombie) che attaccano e sbranano chiunque gli si ponga davanti.
-                           Nel frattempo l'esercito ha creato delle ZQ, cio\u00e8 delle aree murate in cui la popolazione ha ripreso a vivere
-                           e in cui vige il pugno di ferro dell'esercito. Siamo nella ZQ di Boston e un contrabbandiere di nome Joel deve recuperare
-                           un carico di armi che gli \u00e8 stato rubato da un tale Robert, un truffatore noto nella ZQ. Dopo una iniziale colluttazione,
-                           Robert confessa a Joel che le sue armi sono state vendute alle luci. Chi sono le luci? Le luci sono un gruppo di resistenza
-                           (da molti definiti terroristi) che si oppone all'oppressione dell'esercito e cerca di trovare una cura per la pandemia causata dal fungo.
-                           Saputo ci\u00f2, Joel incontra il capo delle luci, una tale Marlene, e spiega l'accaduto per riavere indietro le sue armi.
-                           Marlene propone a Joel un patto: deve portare fuori dalla ZQ, in uno stabilimento delle luci a Nord, una bambina di 12 anni di nome Ellie.
-                           Joel decide, dopo un iniziale rifiuto, di accettare il patto e organizza cos\u00ec il viaggio...""");
-        System.out.println();
-        System.out.println();
-        
-        System.out.println("Boston, 2050");
-        
-        System.out.println();
-        
-        
-        System.out.println(gioco.getStanzaCorrente().getNome());
-        System.out.println();
-        System.out.println(gioco.getStanzaCorrente().getDescrizione());
-        System.out.println();
-        
+        frasi.add("Boston, 2050");
+        frasi.add("\n");
+        frasi.add(gioco.getStanzaCorrente().getNome());
+        frasi.add(gioco.getStanzaCorrente().getDescrizione() + "\n");
         
         //Dialoghi iniziali
+        frasi.add("Ellie:   Ho sentito una sparatoria, cosa è successo?");
+        frasi.add("Joel :   Le luci..");
+        frasi.add("Joel:    Come conosci il capo delle luci?");
+        frasi.add("Ellie:   Marlene conosceva mia mamma e, si è presa cura di me.");
+        frasi.add("Joel:    E così al posto di andare a scuola ti sei unita alle luci.");
+        frasi.add("Ellie:   Non ti dirò perchè mi stai facendo uscire se è questo che vuoi sapere.");
+        frasi.add("Joel:    Sai qual è il bello del mio lavoro? Che non devo sapere il perchè");
+        frasi.add("Joel:    Non me ne frega niente del perchè devi uscire da qui.");
+        frasi.add("Joel:    Muoviamoci, questa storia mi ha già stancato.\n");
         
-        System.out.println("""
-                           Ellie:   Ho sentito una sparatoria, cosa \u00e8 successo?
-                           Joel:    Le luci..
-                           Joel:    Come conosci il capo delle luci?
-                           Ellie:   Marlene conosceva mia mamma e, si \u00e8 presa cura di me.
-                           Joel:    E cos\u00ec al posto di andare a scuola ti sei unita alle luci.
-                           Ellie:   Non ti dir\u00f2 perch\u00e8 mi stai facendo uscire se \u00e8 questo che vuoi sapere.
-                           Joel:    Sai qual \u00e8 il bello del mio lavoro? Che non devo sapere il perch\u00e8.
-                           Joel:    Non me ne frega niente del perch\u00e8 devi uscire da qui. 
-                           Joel:    Muoviamoci, questa storia mi ha gi\u00e0 stancato.
-                           
-                           """);
+        VisualizzatoreFrasi visual = new VisualizzatoreFrasi(frasi);
+        
+        try 
+        {
+            visual.mostraFrasi(1);
+        }
+        catch (InterruptedException ex) 
+        {
+            System.out.println("Riavvia il gioco\n");
+            return;
+        }
+        
         
         /*
         Viene preso un comando in input, viene analizzato dal parser.
