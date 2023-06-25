b0af2fa91cbc07725295d1d2e70eb2cadd626e7f
xLightingBlade <gabrielenigro02@gmail.com>
2023-06-25 23:42:24 +0200

    Rimossi precedenti metodi db

--- src/main/java/com/mycompany/gioco/Avventura.java
+++ src/main/java/com/mycompany/gioco/Avventura.java
@@ -37,9 +37,6 @@ public class Avventura extends StrutturaGioco {
         OperazioniDatabase.connettiDatabase();
         OperazioniDatabase.resetDatabase();
         OperazioniDatabase.creaTabelle();
-        //OperazioniDatabase.popolaTabellaStanze();
-        //OperazioniDatabase.popolaTabellaOggetti();
-        //OperazioniDatabase.popolaTabellaComandi();
         OperazioniDatabase.caricaDati();
         OperazioniDatabase.creaStanze();
         
