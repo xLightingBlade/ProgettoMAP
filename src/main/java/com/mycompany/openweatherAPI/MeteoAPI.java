9233e4987c44d2eb314d99d39738373e71cacc94
xLightingBlade <gabrielenigro02@gmail.com>
2023-06-30 19:13:20 +0200

    Aggiunte dependencies al pom e scritta prima implementazione chiamata API al meteo

---
+++ src/main/java/com/mycompany/openweatherAPI/MeteoAPI.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.openweatherAPI;
+
+import java.io.IOException;
+/**
+ *
+ * @author gabri
+ */
+//Andrebbe migliorato un po', però non credo che l'utente mentre giochi voglia sapere che tempo fa.
+//Semplicemente possiamo scrivere alcune chiamate di nostro gradimento e usarle per mostrare dialoghi diversi in base alle condizioni meteo.
+public class MeteoAPI {
+
+    public static void mostraMeteoCitta(String citta) throws IOException, InterruptedException {
+        //in questo esempio stampa la temperatura. Chiaramente posso chiedergli altro
+        System.out.println(MeteoAPIController.getWeatherByCity(citta).main.temp);
+    }
+    
+    public static void mostraMeteoConCoordinate(String latitudine, String longitudine) throws IOException, InterruptedException {
+        //in questo esempio stampa la temperatura. Chiaramente posso chiedergli altro
+        System.out.println(MeteoAPIController.getWeatherByCoordinates(latitudine, longitudine).main.temp);
+    }
+}
---
+++ src/main/java/com/mycompany/openweatherAPI/MeteoForecastDataTransferObject.java
@@ -0,0 +1,85 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.openweatherAPI;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * Plain Old Java Object, contiene tutti i campi del json restituito dalla chiamata API
+ * @author gabri
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MeteoForecastDataTransferObject {
+    public Coord coord;
+    public ArrayList<Weather> weather;
+    public String base;
+    public Main main;
+    public int visibility;
+    public Wind wind;
+    public Clouds clouds;
+    public int dt;
+    public Sys sys;
+    public int timezone;
+    public int id;
+    public String name;
+    public int cod;
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Clouds{
+        public int all;
+    }
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Coord{
+        public double lon;
+        public double lat;
+    }
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Main{
+        public double temp;
+        public double feels_like;
+        public double temp_min;
+        public double temp_max;
+        public int pressure;
+        public int humidity;
+        public int sea_level;
+        public int grnd_level;
+    }
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Sys{
+        public int type;
+        public int id;
+        public String country;
+        public int sunrise;
+        public int sunset;
+    }
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Weather{
+        public int id;
+        public String main;
+        public String description;
+        public String icon;
+    }
+    
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class Wind{
+        public double speed;
+        public int deg;
+        public double gust;
+    }
+    
+    public static MeteoForecastDataTransferObject parseJson(String json) throws MalformedURLException, IOException {
+        ObjectMapper mapper = new ObjectMapper();
+        return (MeteoForecastDataTransferObject) mapper.readValue(json, MeteoForecastDataTransferObject.class);
+    }
+}
