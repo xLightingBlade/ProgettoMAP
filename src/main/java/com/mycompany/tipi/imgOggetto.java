aeefb4c3a236443782565db2afb6a61f89fe73dd
xLightingBlade <gabrielenigro02@gmail.com>
2023-07-04 18:32:39 +0200

    Da foto a imgfoto

--- src/main/java/com/mycompany/tipi/imgOggetto.java
+++ src/main/java/com/mycompany/tipi/imgOggetto.java
@@ -1,7 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.tipi;
 
 import java.util.Set;
@@ -14,12 +10,11 @@ public class ImgOggetto extends Oggetto {
         
     private String pathImg;
 
-    public ImgOggetto(int id, String nome, String descrizione, Set<String> alias, String contenuto, String relativePathImg) {
-        super(id, nome, descrizione, alias, contenuto);
-        this.pathImg = relativePathImg;
+    public ImgOggetto(int id, String nome, String descrizione) {
+        super(id, nome, descrizione);
     }
     
-    public void setPathImg(String relativePathImg) {
+    public void setImgPath(String relativePathImg) {
         this.pathImg = relativePathImg;
     }
     
