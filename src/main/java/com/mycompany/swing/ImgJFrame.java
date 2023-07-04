a5212289cb2596d04d27cbdabe6831fcf546af07
D4n1l0S4 <d4n1l0.id.apple@gmail.com>
2023-07-04 09:44:31 +0200

    apportate piccole modifiche al progetto

--- src/main/java/com/mycompany/swing/ImgJFrame.java
+++ src/main/java/com/mycompany/swing/ImgJFrame.java
@@ -5,15 +5,10 @@
 package com.mycompany.swing;
 import com.mycompany.exception.ImgException;
 import java.awt.BorderLayout;
-import java.awt.Component;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
-import javax.swing.JOptionPane;
-
 /**
  *
  * @author santo
