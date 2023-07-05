616a7bdc4f1a266cd8aa32fb26520e79f8f2ae59
D4n1l0S4 <d4n1l0.id.apple@gmail.com>
2023-07-05 12:09:04 +0200

    rimosse importazioni non utilizzate

--- src/main/java/com/mycompany/swing/Menu.java
+++ src/main/java/com/mycompany/swing/Menu.java
@@ -16,7 +16,6 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
