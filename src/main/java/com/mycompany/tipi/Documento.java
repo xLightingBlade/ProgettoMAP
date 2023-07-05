3b0256fc21cb7c7edafac47d163b78592d450be1
AntonioMennuni <antoniomennuni97@gmail.com>
2023-07-05 18:58:35 +0200

    completato il setting della mappa. Completat il comando cura. Sviluppato una classe astratta pr il comportamento delle classi che modellano i documenti come immagini o foglietti

---
+++ src/main/java/com/mycompany/tipi/Documento.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tipi;
+
+/**
+ *
+ * @author Antonio
+ */
+public abstract class Documento extends Oggetto 
+{
+    protected String pathDocumento;
+
+    public Documento(int id, String nome, String descrizione) 
+    {
+        super(id, nome, descrizione);
+    }
+    
+    public void setPathDocumento(String pathRelativoDocumento) 
+    {
+        this.pathDocumento = pathRelativoDocumento;
+    }
+    
+    public String getPathDocumento() 
+    {
+        return this.pathDocumento;
+    }
+    
+    public abstract void visualizza();
+}
---
+++ src/main/java/com/mycompany/tipi/OggettoFoglietto.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.tipi;
+
+import com.mycompany.swing.DocumentFrame;
+
+/**
+ *
+ * @author Antonio
+ */
+public class OggettoFoglietto extends Documento
+{
+
+    public OggettoFoglietto(int id, String nome, String descrizione) 
+    {
+        super(id, nome, descrizione);
+    }
+    
+    //Questo metodo si occupa della visualizzazione di un documento di gioco con una interfaccia grafica.
+    //Qualora in futuro non si fosse più soddisfatti del metodo di visualizzazione della foto,
+    //basterà cambiare unicamente il codice di questo metodo.
+    @Override
+    public void visualizza()
+    {     
+        DocumentFrame documentFrame = new DocumentFrame("Documento medico", "Il cordycpes");
+    }
+}
