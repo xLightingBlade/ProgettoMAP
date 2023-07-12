f6da0c744fbd30e5f8e864742877ea50a99319d1
AntonioMennuni <antoniomennuni97@gmail.com>
2023-07-12 17:43:44 +0200

    aggiunto file con i comandi per terminare il gioco. Il file è per il professore.

--- src/main/java/com/mycompany/avventura/CaricamentoDati.java
+++ src/main/java/com/mycompany/avventura/CaricamentoDati.java
@@ -25,7 +25,7 @@ public class CaricamentoDati extends Thread implements Serializable
     @Override
     public void run() 
     {
-        Integer secondiAttesa = 2;
+        Integer secondiAttesa = 1;
         LoaderPrinterCharacterStream frasiIntro = new LoaderPrinterCharacterStream();
         try 
         {
